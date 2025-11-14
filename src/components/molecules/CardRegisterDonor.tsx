9c9b3a8e2af9a3f4bb94fb9c549cda16da004250
moisas <moihgm67@gmail.com>
2025-11-13 22:19:51 -0300

    feat: add tab donation with adjust

--- src/components/molecules/CardRegisterDonor.tsx
+++ src/components/molecules/CardRegisterDonor.tsx
@@ -1,105 +1,135 @@
-import { Controller } from "react-hook-form";
+import { Controller, useFormContext, type FieldValues } from "react-hook-form";
 import { Card } from "../ui/card";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
+import { Spinner } from "../ui/spinner";
 
 interface CardRegisterOngProps {
-  control: any;
   isLoading?: boolean;
   onClickBack?: () => void;
+  onSubmit: (data: FieldValues) => void;
 }
 
 const CardRegisterDonor = ({
-  control,
   isLoading,
   onClickBack,
-}: CardRegisterOngProps) => (
-  <Card className="bg-transparent rounded-sm p-6 max-w-xl mx-auto shadow-lg">
-    <form
-      className="grid grid-cols-1 gap-6 md:grid-cols-2 md:gap-4"
-      onSubmit={(e) => {
-        e.preventDefault();
-      }}
-    >
-      <div className="flex flex-col gap-2 md:col-span-2">
-        <label htmlFor="name" className="text-white font-medium">
-          Nome
-        </label>
-        <Controller
-          name="name"
-          control={control}
-          render={({ field }) => (
-            <Input {...field} placeholder="Digite o nome da instituição" />
-          )}
-        />
-      </div>
-      <div className="flex flex-col gap-2">
-        <label htmlFor="email" className="text-white font-medium">
-          Email
-        </label>
-        <Controller
-          name="email"
-          control={control}
-          render={({ field }) => (
-            <Input
-              {...field}
-              placeholder="Digite o email para contato"
-              type="email"
-            />
-          )}
-        />
-      </div>
-      <div className="flex flex-col gap-2">
-        <label htmlFor="phone" className="text-white font-medium">
-          Telefone
-        </label>
-        <Controller
-          name="phone"
-          control={control}
-          render={({ field }) => (
-            <Input
-              {...field}
-              placeholder="Digite o telefone para contato"
-              type="tel"
-            />
-          )}
-        />
-      </div>
-      <div className="flex flex-col gap-2 md:col-span-2">
-        <label htmlFor="message" className="text-white font-medium">
-          Mensagem
-        </label>
-        <Controller
-          name="message"
-          control={control}
-          render={({ field }) => (
-            <Textarea
-              {...field}
-              placeholder="Digite o motivo de seu desejo de voluntariar"
-              rows={4}
-            />
-          )}
-        />
-      </div>
-      <div className="md:col-span-2 flex flex-col md:flex-row justify-end items-center gap-3 mt-4">
-        <Button
-          type="button"
-          onClick={onClickBack}
-          className="w-full md:w-auto"
+  onSubmit,
+}: CardRegisterOngProps) => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useFormContext<FieldValues>();
+
+  return (
+    <>
+      <Card className="bg-transparent rounded-sm p-6 max-w-xl mx-auto shadow-lg">
+        <form
+          className="grid grid-cols-1 gap-6 md:grid-cols-2 md:gap-4"
+          onSubmit={handleSubmit(onSubmit)}
         >
-          Voltar
-        </Button>
-        <Button
-          className="bg-[#FF9F0D] hover:bg-[#e68a00] px-6 py-2 font-semibold w-full md:w-auto"
-          type="submit"
-          disabled={isLoading}
-        >
-          Enviar Solicitação
-        </Button>
-      </div>
-    </form>
-  </Card>
-);
+          <div className="flex flex-col gap-2 md:col-span-2">
+            <label htmlFor="name" className="text-white font-medium">
+              Nome
+            </label>
+            <Controller
+              name="name"
+              control={control}
+              render={({ field }) => (
+                <Input {...field} placeholder="Digite o nome da instituição" />
+              )}
+            />
+            {errors.name && (
+              <span className="text-red-500 text-sm">
+                {errors.name.message as string}
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="email" className="text-white font-medium">
+              Email
+            </label>
+            <Controller
+              name="email"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  placeholder="Digite o email para contato"
+                  type="email"
+                />
+              )}
+            />
+            {errors.email && (
+              <span className="text-red-500 text-sm">
+                {errors.email.message as string}
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="phone" className="text-white font-medium">
+              Telefone
+            </label>
+            <Controller
+              name="phone"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  placeholder="Digite o telefone para contato"
+                  type="tel"
+                />
+              )}
+            />
+            {errors.phone && (
+              <span className="text-red-500 text-sm">
+                {errors.phone.message as string}
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col gap-2 md:col-span-2">
+            <label htmlFor="message" className="text-white font-medium">
+              Mensagem
+            </label>
+            <Controller
+              name="message"
+              control={control}
+              render={({ field }) => (
+                <Textarea
+                  {...field}
+                  placeholder="Digite o motivo de seu desejo de voluntariar"
+                  rows={4}
+                />
+              )}
+            />
+            {errors.message && (
+              <span className="text-red-500 text-sm">
+                {errors.message.message as string}
+              </span>
+            )}
+          </div>
+          <div className="md:col-span-2 flex justify-end mt-2 gap-2">
+            <Button onClick={onClickBack}>Voltar</Button>
+            {!isLoading && (
+              <Button
+                className="bg-[#FF9F0D] hover:bg-[#e68a00] px-6 py-2 font-semibold"
+                type="submit"
+                disabled={isLoading}
+              >
+                Registrar Instituição
+              </Button>
+            )}
+            {isLoading && (
+              <div className="bg-[#FF9F0D] rounded-md text-white hover:bg-[#e68a00] px-6 py-2 font-semibold">
+                <Spinner color="white" />
+              </div>
+            )}
+          </div>
+        </form>
+      </Card>
+    </>
+  );
+};
 
 export default CardRegisterDonor;
--- src/components/templates/Donation.tsx
+++ src/components/templates/Donation.tsx
@@ -1,33 +1,57 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { toast, ToastContainer } from "react-toastify";
 
 import { Button } from "../ui/button";
 import { Card, CardContent, CardDescription, CardTitle } from "../ui/card";
+import { useOngCreate } from "@/hooks/useOng";
 
 import { type OngFormValues, ongSchema } from "@/schemas/ongFormSchema";
 import { type DonorFormValues, donorSchema } from "@/schemas/donorFormSchema";
 
 import CardRegisterDonor from "../molecules/CardRegisterDonor";
 import CardRegisterOng from "../molecules/CardRegisterOng";
+import CardSuccess from "../molecules/CardSuccess";
 
 function Donation() {
   const [activeTab, setActiveTab] = useState<"donor" | "ong" | "home">("home");
+  const [showSuccessOng, setShowSuccessOng] = useState(false);
+  const [showSuccessDonor, setShowSuccessDonor] = useState(false);
 
-  const { control: ongControl, handleSubmit: ongHandleSubmit } =
-    useForm<OngFormValues>({
-      resolver: zodResolver(ongSchema),
-      defaultValues: {
-        name: "",
-        email: "",
-        phone: "",
-        cnpj: "",
-        description: "",
-        address: "",
-      },
-    });
+  const { mutateAsync: createOng, isPending, isError } = useOngCreate();
 
-  const { control: donorControl } = useForm<DonorFormValues>({
+  useEffect(() => {
+    if (isError) {
+      toast.error("Ong já cadastrada!", {
+        theme: "dark",
+        position: "top-center",
+      });
+    }
+  }, [isError]);
+
+  useEffect(() => {
+    if (activeTab !== "ong") {
+      setShowSuccessOng(false);
+    }
+    if (activeTab !== "donor") {
+      setShowSuccessDonor(false);
+    }
+  }, [activeTab]);
+
+  const fieldsOng = useForm<OngFormValues>({
+    resolver: zodResolver(ongSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+      cnpj: "",
+      description: "",
+      address: "",
+    },
+  });
+
+  const fieldsDonor = useForm<DonorFormValues>({
     resolver: zodResolver(donorSchema),
     defaultValues: {
       name: "",
@@ -37,60 +61,121 @@ function Donation() {
     },
   });
 
+  const handleOngSubmit = async (data: OngFormValues) => {
+    if (!data) return;
+    try {
+      await createOng(data);
+      fieldsOng.reset();
+      setShowSuccessOng(true);
+    } catch {
+      setShowSuccessOng(false);
+    }
+  };
+
+  const handleDonorSubmit = async (data: DonorFormValues) => {
+    if (!data) return;
+    try {
+      fieldsDonor.reset();
+      setShowSuccessDonor(true);
+    } catch {
+      setShowSuccessDonor(false);
+    }
+  };
+
+  const handleSuccessBackOng = () => {
+    setShowSuccessOng(false);
+    setActiveTab("home");
+  };
+
+  const handleSuccessBackDonor = () => {
+    setShowSuccessDonor(false);
+    setActiveTab("home");
+  };
+
   return (
-    <main className="container mx-auto min-h-screen flex items-center justify-center px-4 sm:px-6 md:px-8 py-8">
-      {activeTab === "donor" && (
-        <CardRegisterDonor
-          control={donorControl}
-          isLoading={false}
-          onClickBack={() => setActiveTab("home")}
-        />
-      )}
-      {activeTab === "ong" && (
-        <CardRegisterOng
-          control={ongControl}
-          isLoading={false}
-          onSubmit={() =>
-            ongHandleSubmit((data) => {
-              console.log(data);
-            })
-          }
-          onClickBack={() => setActiveTab("home")}
-        />
-      )}
-      {activeTab === "home" && (
-        <Card className="bg-transparent rounded-sm w-full max-w-lg mx-auto">
-          <CardContent className="flex flex-col gap-4 items-center justify-center w-full px-2 sm:px-4 md:px-8">
-            <img
-              src="https://res.cloudinary.com/dtglxnl4z/image/upload/v1763076077/bro_zo85fp.png"
-              className="w-full max-w-xs sm:max-w-sm md:max-w-md h-auto rounded-lg object-cover"
-              alt="Ilustração combate à fome"
-            />
-            <CardTitle className="flex justify-center text-center text-[#FF9F0D] font-bold text-2xl md:text-3xl">
-              Faça parte da rede que combate a fome no Brasil.
-            </CardTitle>
-            <CardDescription className="text-white text-sm md:text-base text-center">
-              Conecte-se a ONGs reais que mudam vidas todos os dias. Cadastre
-              sua organização ou contribua como doador.
-            </CardDescription>
-          </CardContent>
-          <CardContent className="flex flex-col sm:flex-row justify-center gap-3 sm:gap-4 w-full px-2 sm:px-4 md:px-8">
-            <Button
-              onClick={() => setActiveTab("donor")}
-              className="bg-[#FF9F0D] font-bold rounded-sm w-full sm:w-auto px-6 py-3 mt-2 text-base transition-colors duration-200 hover:bg-yellow-400 focus:ring-2 focus:ring-yellow-500 focus:outline-none"
-            >
-              Sou doador
-            </Button>
-            <Button
-              onClick={() => setActiveTab("ong")}
-              className="bg-[#FF9F0D] font-bold rounded-sm w-full sm:w-auto px-6 py-3 mt-2 text-base transition-colors duration-200 hover:bg-yellow-400 focus:ring-2 focus:ring-yellow-500 focus:outline-none"
-            >
-              Sou uma ONG
-            </Button>
-          </CardContent>
-        </Card>
-      )}
-    </main>
+    <>
+      <main
+        className="min-h-screen flex items-center justify-center bg-transparent px-2 sm:px-0"
+        style={{ width: "100%" }}
+      >
+        <ToastContainer />
+        {activeTab === "donor" && (
+          <FormProvider {...fieldsDonor}>
+            {!showSuccessDonor && (
+              <div className="w-full max-w-md mx-auto">
+                <CardRegisterDonor
+                  isLoading={false}
+                  onClickBack={() => setActiveTab("home")}
+                  onSubmit={() => handleDonorSubmit(fieldsDonor.getValues())}
+                />
+              </div>
+            )}
+            {showSuccessDonor && (
+              <div className="w-full max-w-md mx-auto">
+                <CardSuccess
+                  textMode="donor"
+                  onClickBack={handleSuccessBackDonor}
+                />
+              </div>
+            )}
+          </FormProvider>
+        )}
+        {activeTab === "ong" && (
+          <FormProvider {...fieldsOng}>
+            {!showSuccessOng && (
+              <div className="w-full max-w-md mx-auto">
+                <CardRegisterOng
+                  isLoading={isPending}
+                  onClickBack={() => setActiveTab("home")}
+                  onSubmit={() => handleOngSubmit(fieldsOng.getValues())}
+                />
+              </div>
+            )}
+            {showSuccessOng && (
+              <div className="w-full max-w-md mx-auto">
+                <CardSuccess
+                  textMode="ong"
+                  onClickBack={handleSuccessBackOng}
+                />
+              </div>
+            )}
+          </FormProvider>
+        )}
+        {activeTab === "home" && (
+          <Card className="bg-transparent rounded-sm w-full max-w-md mx-auto">
+            <CardContent className="flex flex-col gap-2 items-center justify-center w-full">
+              <img
+                src="https://res.cloudinary.com/dtglxnl4z/image/upload/v1763076077/bro_zo85fp.png"
+                className="max-w-sm sm:max-w-sm h-auto rounded-lg object-cover"
+                alt=""
+                style={{ aspectRatio: "1.5/1" }}
+              />
+              <CardTitle className="flex justify-center text-center text-[#FF9F0D] font-bold text-xl sm:text-2xl">
+                Faça parte da rede que combate a fome no Brasil.
+              </CardTitle>
+              <CardDescription className="text-white text-sm text-center px-2">
+                Conecte-se a ONGs reais que mudam vidas todos os dias. Cadastre
+                sua organização ou contribua como doador.
+              </CardDescription>
+            </CardContent>
+            <CardContent className="flex flex-col sm:flex-row justify-center gap-2 sm:gap-4 w-full px-2">
+              <Button
+                onClick={() => setActiveTab("donor")}
+                className="bg-[#FF9F0D] font-bold rounded-sm w-full sm:w-fit px-6 py-3 mt-2 text-base transition-colors duration-200 hover:bg-yellow-400 focus:ring-2 focus:ring-yellow-500 focus:outline-none"
+              >
+                Sou doador
+              </Button>
+              <Button
+                onClick={() => setActiveTab("ong")}
+                className="bg-[#FF9F0D] font-bold rounded-sm w-full sm:w-fit px-6 py-3 mt-2 text-base transition-colors duration-200 hover:bg-yellow-400 focus:ring-2 focus:ring-yellow-500 focus:outline-none"
+              >
+                Sou uma ONG
+              </Button>
+            </CardContent>
+          </Card>
+        )}
+      </main>
+    </>
   );
 }
 export default Donation;
